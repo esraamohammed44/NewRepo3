96c65f5cda9a444e7fa4f92b55eb84e174cb2df5
esraamohammed44 <esraamohammed1112000@gmail.com>
2024-05-13 14:04:01 +0200

    Add project files.

---
+++ AlHelal_Store/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlHelal_Store.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AlHelal_Store/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlHelal_Store.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
